c15283322581b1ba39171609f2545459aa74f725
Deepthansu G V S <deepthansugvs@nothing.local>
2021-12-13 01:06:49 +0530

    PropFile

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager, User
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from django.conf import settings
 from django.db.models.signals import post_save
