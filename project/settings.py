256ba0bf6db2452ce0932863e4496cfbae5fc03c
Deepthansu G V S <deepthansugvs@nothing.local>
2021-12-13 01:39:30 +0530

    PropFile

--- project/settings.py
+++ project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,7 +44,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'corsheaders',
-    'accounts',
     'django_extensions',
     'djoser',
 ]
@@ -92,6 +92,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'dbsqlite.db',
+#     }
+# }
 
 DATABASES = {
     'default': {
