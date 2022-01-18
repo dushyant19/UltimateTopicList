acb62b9cab9fca44eedde0181eda3294f17b3fc5
Deepthansu G V S <deepthansugvs@nothing.local>
2022-01-18 18:34:09 +0530

    change the host link

--- project/settings.py
+++ project/settings.py
@@ -165,8 +165,9 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 from datetime import timedelta
 SIMPLE_JWT = {
-  'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
-  'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
+    #change later
+  'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+  'REFRESH_TOKEN_LIFETIME': timedelta(minutes=2),
   'ROTATE_REFRESH_TOKENS': False,
   'BLACKLIST_AFTER_ROTATION': True,
   'UPDATE_LAST_LOGIN': False,
