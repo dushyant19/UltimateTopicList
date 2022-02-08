d135e77a8be8af6954169974f34d29d5f720cf58
Deepthansu G V S <deepthansugvs@MacBook-Air.local>
2022-02-08 17:38:22 +0530

    Structure Defined

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,3 +6,11 @@ urlpatterns = [
     path('',include('djoser.urls.jwt')),
     path('check/',UserExistsCheck.as_view(),name='user_check')
 ]
+
+
+"""
+1. Logged In = Home, Topics, Solved, Logout
+2. Not Logged In = Home, Topics, ForgotPassword, SignUp, Login, Activation, 
+3. Not Redundant = Topics, 
+
+"""
